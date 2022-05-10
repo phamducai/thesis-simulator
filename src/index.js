b9156bfcf2e4177eeb7a51a7becf83dc80da5ff3
phamducai <giacat2910@gmail.com>
2022-05-10 14:56:50 +0700

    hah

--- src/index.js
+++ src/index.js
@@ -17,31 +17,81 @@ client.on("connect", () => {
   });
 
   // code sau danh cho provision, chi chay 1 lan
+
+  //relay ADe
+  // const msgObj = {
+  //   action: "provision",
+  //   deviceName: "May bom",
+  //   deviceType: "RelayAde",
+  //   channels: {
+  //     Relay_state: true,
+  //     vrms: 220,
+  //     irms: 10,
+  //     power: 110,
+  //     enegy: 310,
+  //   },
+  //   refRoom: "627411c4fdef6aa7eb4705aa",
+  // };
+  // client.publish("up/", JSON.stringify(msgObj));
+  // //sensor add
+  // const msgObj = {
+  //   deviceName: "Sensor",
+  //   action: "provision",
+  //   deviceType: "Sensor",
+  //   channels: {
+  //     temp: 110,
+  //     humidity: 220,
+  //     airquality: 10,
+  //   },
+  //   refRoom: "627411c4fdef6aa7eb4705aa",
+  // };
+  // client.publish("up/", JSON.stringify(msgObj));
+
+  // Relay3Channels;
   // const msgObj = {
   //   action: "provision",
   //   deviceName: "Relay 3 Channels",
   //   deviceType: "Relay3Channels",
   //   channels: {
-  //     channel1: 0,
-  //     channel2: 0,
-  //     channel3: 0,
+  //     namechannel1: "loai 1",
+  //     channel1: 4123,
+  //     namechannel2: "lhasd",
+  //     channel2: 41234,
+  //     namechannel3: "ccc",
+  //     channel3: 12421,
   //   },
+  //   refRoom: "627411c4fdef6aa7eb4705aa",
   // };
+
   // client.publish("up/", JSON.stringify(msgObj));
 
-  // code sau cho telemetry
+  /////////////////////////////////////////////////////////////////////////////////////////////code sau cho telemetry
   // khi nao provision thi phai comment no di
 
+  // setInterval(() => {
+  //   const deviceId = "62789eec08a7577efaf71c6d";
+  //   const msgObj = {
+  //     action: "telemetry",
+  //     channels: {
+  //       channel1: getRandomInt(110, 220),
+  //       channel2: getRandomInt(110, 220),
+  //       channel3: getRandomInt(110, 220),
+  //     },
+  //   };
+  //   client.publish("up/" + deviceId, JSON.stringify(msgObj));
+  // }, 2000);
+  //Ade relay
   setInterval(() => {
+    const deviceId = "6278c48a12ccdde504ae8f3e";
     const msgObj = {
       action: "telemetry",
       channels: {
-        channel0: getRandomInt(110, 220),
-        channel1: getRandomInt(110, 220),
-        channel3: getRandomInt(110, 220),
+        vrms: getRandomInt(110, 220),
+        irms: getRandomInt(110, 220),
+        energy: getRandomInt(110, 220),
       },
     };
-    client.publish("up/6276b3031269a1c923ff50be", JSON.stringify(msgObj));
+    client.publish("up/" + deviceId, JSON.stringify(msgObj));
   }, 2000);
 });
 
